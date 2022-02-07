089a6e064b765baf42f525762019cbecc5ce81b2
Kevin Zhou <bigsoupzhou@Gmail.com>
2022-02-07 19:15:41 +1100

    initial commit

---
+++ store/models.py
@@ -0,0 +1,66 @@
+import email
+from pyexpat import model
+from django.db import models
+
+# Create your models here.
+
+
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey(
+        'Product', on_delete=models.SET_NULL, null=True, related_name='+')
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory_type = models.IntegerField()
+    last_updated = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateTimeField(null=True)
+    MEMBERSHIP_CHOICES = [
+        ('B', 'Bronze'),
+        ('S', 'Silver'),
+        ('G', 'Gold'),
+    ]
+    membership = models.CharField(
+        max_length=1, choices=MEMBERSHIP_CHOICES, default='B')
+
+
+class Order(models.Model):
+    PAYMENT_STATUS_CHOICES = [
+        ('P', 'Pending'),
+        ('C', 'Complete'),
+        ('F', 'Failed')
+    ]
+    placed_at = models.DateTimeField(auto_now=True)
+    payment_status = models.CharField(
+        max_length=1, choices=PAYMENT_STATUS_CHOICES, default='P')
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
